5617542ee6fe49ba7156808cfc5a648437f2f542
Erica Nafziger <garbererica@gmail.com>
2017-03-01 16:18:43 -0800

    add mobile navigation flyout - not yet triggered by anything in JS

--- js/scripts.js
+++ js/scripts.js
@@ -5,47 +5,14 @@ $(document).ready(function() {
 
     /* Check the location of each animate-in elements */
     $('.animate-in').each( function(i){
-
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 200;
+      var top_of_object = $(this).offset().top;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* If the object is completely visible in the window, fade it it */
       if( bottom_of_window > bottom_of_object ){
-
-        $(this).animate({'opacity':'1'},400);
+        $(this).animate({'opacity':'1'},800);
       }
-
     });
-
-    /* Check the location of animate-left elements */
-    $('.animate-left').each( function(i){
-
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* If the object is completely visible in the window, move it it */
-      if( bottom_of_window > bottom_of_object ){
-
-        $(this).animate({'margin-left':'0', 'opacity':'1'},600);
-
-      }
-
-    });
-    /* Check the location of animate-right elements */
-    $('.animate-right').each( function(i){
-
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* If the object is completely visible in the window, move it it */
-      if( bottom_of_window > bottom_of_object ){
-
-        $(this).animate({'margin-right':'0', 'opacity':'1'},600);
-
-      }
-
-    });
-
   });
-
 });
