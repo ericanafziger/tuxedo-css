f1c5ffd2a0ff95db8e7585e2cbce3ae174699cfd
Erica Nafziger <garbererica@gmail.com>
2017-03-02 09:42:12 -0800

    refactor tuxedo.js with callback functions to run animate and mobile nav

---
+++ js/tuxedo.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  /* Every time the window is scrolled ... */
+  function animations() {
+    $(window).scroll( function(){
+
+      /* Check the location of each animate-in elements */
+      $('.animate-in').each( function(i){
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 200;
+        var top_of_object = $(this).offset().top;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},800);
+        }
+      });
+    });
+  }
+  function mobileMenu() {
+    $('.hamburger').click(function() {
+      $('.flyout').show();
+      $('body').css({'overflow': 'hidden'});
+    });
+    $('.close').click(function() {
+      $('.flyout').hide();
+      $('body').css({'overflow': 'auto'});
+    });
+    $('.fly-dropdown').click(function() {
+      $(this).children('.dropdown-menu').fadeToggle();
+    });
+    $('.sub-fly-dropdown').hover(function() {
+      $(this).children('.sub-dropdown-menu').fadeToggle();
+    });
+  }
+
+////////// Add Functions to Call HERE//////////
+
+  mobileMenu();
+  animations();
+});
