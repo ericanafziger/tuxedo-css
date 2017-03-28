d69836ae9a2c29ec2e774eeccfb17bd431a6f40e
Erica Nafziger <garbererica@gmail.com>
2017-03-27 19:47:18 -0700

    refactor scrollSpy function

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
+  var vars = {};
+  var stagesNumber = $('[id*="stage"]').find().prevObject.length; //counts stages on each page
+  for (var i = 1; i <= stagesNumber; i++) { //creates variable for each stage
+    vars['stageTop' + i] = $('#stage' + i).offset().top;
+  }
+
+  //scroll spy functionality
+  function scrollSpy() {
+    var windowTop = $(window).scrollTop() + 350;
+    for (var j = 1; j <= stagesNumber; j++) {
+      if (windowTop >= (vars['stageTop'+j]) && windowTop < (vars['stageTop'+(j+1)])) {
+        $('a[href*="#stage"]').parent().removeClass('active');
+        $('a[href*="#stage'+j+'"]').parent().addClass('active');
+      } else if (windowTop >= (vars['stageTop'+stagesNumber])){
+        $('a[href*="#stage"]').parent().removeClass('active');
+        $('a[href*="#stage'+stagesNumber+'"]').parent().addClass('active');
+      }
+    };
+  }
+  scrollSpy(); //calls function on page load
+
+  $(window).scroll(function() { //calls function on scroll
+    scrollSpy();
+  });
+  
   //smooth scrolls to different stages on page
   $('a[href*="#stage"]').click(function() { /* class or id of button/link that will be clicked */
     var number = this.href.replace(/\D+/g, '');
@@ -9,26 +34,4 @@ $(document).ready(function() {
       }, 1000);
   });
 
-
-  //scroll spy functionality
-  var vars = {};
-  var stagesNumber = $('[id*="stage"]').find().prevObject.length;
-  console.log(stagesNumber);
-  for (var i = 1; i <= stagesNumber; i++) {
-    vars['stageTop' + i] = $('#stage' + i).offset().top;
-  }
-  console.log(vars);
-  $(window).scroll(function() {
-    var windowTop = $(window).scrollTop() + 350;
-    for (var j = 1; j <= stagesNumber; j++) {
-      console.log('windowTop: '+windowTop+ ' stage7: ' +vars.stageTop7);
-      if (windowTop >= (vars['stageTop'+j]) && windowTop < (vars['stageTop'+(j+1)])) {
-        $('a[href*="#stage"]').parent().removeClass('active');
-        $('a[href*="#stage'+j+'"]').parent().addClass('active');
-      } else if (windowTop >= (vars['stageTop'+stagesNumber])){
-        $('a[href*="#stage"]').parent().removeClass('active');
-        $('a[href*="#stage'+stagesNumber+'"]').parent().addClass('active');
-      }
-    };
-  });
 });
