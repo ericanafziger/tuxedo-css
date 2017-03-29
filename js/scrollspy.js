f4b2747b69efb20f3a61ea4cb6f6e000e9e2a888
Erica Nafziger <garbererica@gmail.com>
2017-03-28 19:59:42 -0700

    fix scroll spy bug with left border jump before scroll position is set

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
   var vars = {};
-  var stagesNumber = $('[id*="stage"]').find().prevObject.length; //counts stages on each page
-  for (var i = 1; i <= stagesNumber; i++) { //creates variable for each stage
-    vars['stageTop' + i] = $('#stage' + i).offset().top;
+  var sectionNumber = $('[id*="section"]').find().prevObject.length; //counts stages on each page
+  for (var i = 1; i <= sectionNumber; i++) { //creates variable for each stage
+    vars['sectionTop' + i] = $('#section' + i).offset().top;
   }
-
+  console.log(sectionNumber);
   //scroll spy functionality
   function scrollSpy() {
     var windowTop = $(window).scrollTop() + 350;
-    for (var j = 1; j <= stagesNumber; j++) {
-      if (windowTop >= (vars['stageTop'+j]) && windowTop < (vars['stageTop'+(j+1)])) {
-        $('a[href*="#stage"]').parent().removeClass('active');
-        $('a[href*="#stage'+j+'"]').parent().addClass('active');
-      } else if (windowTop >= (vars['stageTop'+stagesNumber])){
-        $('a[href*="#stage"]').parent().removeClass('active');
-        $('a[href*="#stage'+stagesNumber+'"]').parent().addClass('active');
+    for (var j = 1; j <= sectionNumber; j++) {
+      if (windowTop >= (vars['sectionTop'+j]) && windowTop < (vars['sectionTop'+(j+1)])) {
+        $('[id*="scrollSpy"]').removeClass('active');
+        $('#scrollSpy'+j).addClass('active');
+      } else if (windowTop >= (vars['sectionTop'+sectionNumber])){
+        $('[id*="scrollSpy"]').removeClass('active');
+        $('#scrollSpy'+sectionNumber).addClass('active');
       }
     };
   }
@@ -24,12 +24,12 @@ $(document).ready(function() {
   $(window).scroll(function() { //calls function on scroll
     scrollSpy();
   });
-  
+
   //smooth scrolls to different stages on page
-  $('a[href*="#stage"]').click(function() { /* class or id of button/link that will be clicked */
-    var number = this.href.replace(/\D+/g, '');
+  $('[id*="scrollSpy"]').click(function() { /* class or id of button/link that will be clicked */
+    var number = this.id.replace(/\D+/g, '');
       $('html, body').animate({
-          scrollTop: $("#stage" + number).offset().top, /* class or id of div that will be scrolled to */
+          scrollTop: $("#section" + number).offset().top, /* class or id of div that will be scrolled to */
           behavior: 'smooth',
       }, 1000);
   });
